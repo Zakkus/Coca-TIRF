5caeaf4a6c98493378fe4e3ef5f255b4c3b86142
Jérémy Forget <forget_j@epita.fr>
2015-06-19 14:53:00 +0200

    compo change

--- Filters.cc
+++ Filters.cc
@@ -380,7 +380,7 @@ void Compo_tb(SDL_Surface* image, int i, int j, int n1, int n2, int n3)
 				Compo_lr(image, i + 1, k, n1, n2, n3);*/
 			Compo_lr(image, i, k, n1, n2, n3);
 		}
-		for (int l = j + 1; l < width && getRGB(image, i, l)[0] == 0 && getRGB(image, i, l)[1] == 0 && getRGB(image, i, l)[2] == 0; l++)
+		for (int l = j + 1; l < height && getRGB(image, i, l)[0] == 0 && getRGB(image, i, l)[1] == 0 && getRGB(image, i, l)[2] == 0; l++)
 		{
 			//setPixel(image, i, l, SDL_MapRGB(image->format, n1, n2, n3));
 			/*if (i - 1 >= 0)
