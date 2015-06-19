361e64f4e21853fd8d69bb8e372c5d4099250585
Jérémy Forget <forget_j@epita.fr>
2015-06-19 15:56:26 +0200

    compo_bl2.0 surement segf

--- Filters.cc
+++ Filters.cc
@@ -398,15 +398,15 @@ void Compo_tr(SDL_Surface* image, int i, int j, int n1, int n2, int n3)
     int height = image->h;
 	int lastx = i - 1;
 	int lasty = j - 1;
-	if (lastx >= 0 && getRGB(image, lastx, j)[0] == 0 && getRGB(image, lastx, j)[1] == 0 && getRGB(image, lastx, j)[2] == 0)
+	if (lastx >= 0 && getRGB(image, lastx, j)[0] != 255 && getRGB(image, lastx, j)[1] != 255 && getRGB(image, lastx, j)[2] != 255 && getRGB(image, lastx, j)[0] != n1 && getRGB(image, lastx, j)[1] != n2 && getRGB(image, lastx, j)[2] != n3)
 		Compo_bl(image, lastx, j, n1, n2, n3);
-	if (lasty >=0 && getRGB(image, i, lasty)[0] == 0 && getRGB(image, i, lasty)[1] == 0 && getRGB(image, i, lasty)[2] == 0)
+	if (lasty >=0 && getRGB(image, i, lasty)[0] != 255 && getRGB(image, i, lasty)[1] != 255 && getRGB(image, i, lasty)[2] != 255 && getRGB(image, i, lasty)[0] != n1 && getRGB(image, i, lasty)[1] != n2 && getRGB(image, i, lasty)[2] != n3)
 		Compo_bl(image, i, lasty, n1, n2, n3);
-	for (int k = i; k < width && getRGB(image, k, j)[0] == 0 && getRGB(image, k, j)[1] == 0 && getRGB(image, k, j)[2] == 0; k++)
+	for (int k = i; k < width && && getRGB(image, k, j)[0] != 255 && getRGB(image, k, j)[1] != 255 && getRGB(image, k, j)[2] != 255 && getRGB(image, k, j)[0] != n1 && getRGB(image, k, j)[1] != n2 && getRGB(image, k, j)[2] != n3; k++)
 	{
 		setPixel(image, k, j, SDL_MapRGB(image->format, n1, n2, n3));
 		int l = j + 1;
-		if (l < height && getRGB(image, k, l)[0] == 0 && getRGB(image, k, l)[1] == 0 && getRGB(image, k, l)[2] == 0)
+		if (l < height && getRGB(image, k, l)[0] != 255 && getRGB(image, k, l)[1] != 255 && getRGB(image, k, l)[2] != 255 && getRGB(image, k, l)[0] != n1 && getRGB(image, k, l)[1] != n2 && getRGB(image, k, l)[2] != n3)
 			Compo_tr(image, k, l, n1, n2, n3);
 	}
 }
